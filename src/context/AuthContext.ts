58f172e9b51e317e96458cd7b215845c4eb888aa
lemmyosborne <bozhenko.rodion@mail.ru>
2022-03-05 01:50:55 +0300

    feat: add useUser hook which return a current user and function which can change him

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,9 +1,11 @@
-import { createContext, SetStateAction, Dispatch } from "react"
+import { createContext, SetStateAction, Dispatch, useContext } from "react"
 import { CognitoUser } from "@aws-amplify/auth"
 
-interface IAuthContext {
+export interface IAuthContext {
   user: CognitoUser | null
   setUser: Dispatch<SetStateAction<CognitoUser | null>>
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext)
+
+export const useUser = (): IAuthContext => useContext(AuthContext)
