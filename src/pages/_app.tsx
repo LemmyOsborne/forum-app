a62cc85081f16499c89d4db283292f37e087a6c3
lemmyosborne <bozhenko.rodion@mail.ru>
2022-03-09 17:06:58 +0300

    deploy: change the header file name in order to fix deployment error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { GlobalStyle } from "styles/global-style"
 import { Amplify } from "aws-amplify"
 import awsconfig from "../aws-exports"
 import { AuthProvider } from "helpers/AuthProvider"
-import { Header } from "components/header"
+import { Header } from "components/Header"
 import { ToggleThemeContext } from "context/ToggleThemeContext"
 
 Amplify.configure({ ...awsconfig, ssr: true })
